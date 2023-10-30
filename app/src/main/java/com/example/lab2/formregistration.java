6e464be018d0eb137f233bf6befcafffc4d0727e
isco323 <norev1997@mail.ru>
2023-10-30 14:21:22 +0300

    fvlab

--- app/src/main/java/com/example/lab2/formregistration.java
+++ app/src/main/java/com/example/lab2/formregistration.java
@@ -1,5 +1,6 @@
 package com.example.lab2;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,18 +11,34 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 
+import java.util.List;
+
 
 public class formregistration extends Activity {
-    Button buttonenter, buttonleave;
+    Button buttonenter, buttonleave, buttonload;
+    EditText Email, password;
+    DatabaseHandler db = new DatabaseHandler(this);
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.enterleave);
         buttonenter = findViewById(R.id.buttonenter);
         buttonleave = findViewById(R.id.buttonleave);
-        EditText Email = findViewById(R.id.editTextTextEmailAddress);
-        EditText password = findViewById(R.id.editTextTextPassword);
+        buttonload = findViewById(R.id.buttonload);
+        Email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
         Intent openIntent = new Intent(formregistration.this, DataDynamic.class);
+        buttonload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<User> users = db.getAllUsers();
+                for (User usr : users) {
+                    String log = "Id: "+usr.getID()+" ,Login: " + usr.getLogin() + " ,Password: " + usr.getPass();
+                    Log.v("Loading...", log);
+                }
+            }
+        });
         buttonenter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,11 +46,11 @@ public class formregistration extends Activity {
                 {
                     String data;
                     data = "Email: " + Email.getText().toString() + "\nPassword: "+ password.getText().toString();
+                    db.addUser (new User(Email.getText().toString(), password.getText().toString()));
                     openIntent.putExtra("data",data);
                     startActivity(openIntent);
                 }
             }
-
             private boolean check() {
                 Toast mytoast = new Toast(formregistration.this);
                 if(Email.getText().toString().equals(""))
@@ -55,6 +72,12 @@ public class formregistration extends Activity {
             public void onClick(View view) {
                 Email.setText("");
                 password.setText("");
+                db.deleteUser(new User(Email.getText().toString(), password.getText().toString()));
+                List<User> users = db.getAllUsers();
+                for (User usr : users) {
+                    String log = "Id: "+usr.getID()+" ,Login: " + usr.getLogin() + " ,Password: " + usr.getPass();
+                    Log.v("Loading...", log);
+                }
             }
         });
     }
@@ -65,5 +88,10 @@ public class formregistration extends Activity {
         Log.i("Logs", "app started");
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+    }
 
 }
